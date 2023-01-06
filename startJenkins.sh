0612e2ab953f5e4f634c68b8e99fd3c4b84415c6
blurdel <5648737+blurdel@users.noreply.github.com>
2023-01-06 16:22:38 -0500

    Add startJenkins.sh

---
+++ startJenkins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pid=$( ps -ef | grep -v grep | grep jenkins | awk '{print $2}' )
+if [[ ! -z ${pid} ]]; then
+    echo "Jenkins is already running, pid=${pid}"
+    exit 1
+fi
+
+cd ~/Programs/jenkins-jar
+JENKINS_HOME=~/.jenkins  java -Dhudson.plugins.git.GitSCM.ALLOW_LOCAL_CHECKOUT=true -jar jenkins.war &
+
