9dbd05aab6997e36ab6b7a3a6e8442367d831361
blurdel <5648737+blurdel@users.noreply.github.com>
2023-01-17 19:09:45 -0500

    Updated script to take job-name and branch arguments

--- getJenkinsLastBuild.sh
+++ getJenkinsLastBuild.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+me=$(basename $0)
+# echo "me: ${me} $@"
+
+if [[ $# -lt 1 || $# -gt 2 ]]; then
+    echo "usage: $0 <job-name> [branch]"
+    exit 1
+fi
+
+jobname="${1}"
+branch="${2:-main}"
+echo "Running ${jobname} ${branch}"
+
 # Encode the URL
-URL="http://localhost:8080/job/jenkins-hello-world/job/main/lastSuccessfulBuild/api/json"
+URL="http://localhost:8080/job/${jobname}/job/${branch}/lastSuccessfulBuild/api/json"
 echo "URL: ${URL}"
 
 curl -X POST -vL --user auto:"$(< auto.TOKEN)"  "${URL}" | jq -r ".id, .timestamp, .url"
